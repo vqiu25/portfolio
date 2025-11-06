4e71aefcb8add8a1e5425e3abae882af056f1d49
Victor Esther Qiu <vqiu25@gmail.com>
2025-11-07 07:51:33 +1300

    ✨ footer template

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Projects from "./components/app/projects/Projects";
 import Experience from "./components/app/experience/Experience";
 import Contact from "./components/app/contact/Contact";
 import Line from "./components/utility/line/Line";
+import Footer from "./components/app/footer/Footer";
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
         <Experience />
         <Line />
         <Contact />
+        <Footer />
       </div>
     </>
   );
--- src/components/app/contact/Contact.tsx
+++ src/components/app/contact/Contact.tsx
@@ -1,9 +1,36 @@
 import Heading from "../../utility/heading/Heading";
+import styles from "./Contact.module.css";
 
 export default function Contact() {
   return (
-    <div>
-      <Heading>Contact</Heading>
-    </div>
+    <section className={styles.contact}>
+      <div className={styles.left}>
+        <Heading>Contact</Heading>
+        <h2 className={styles.location}>
+          Presently located in <br />
+          <span className={styles.city}>Auckland, NZ</span>
+        </h2>
+      </div>
+
+      <div className={styles.right}>
+        <a
+          href="mailto:victorqiu.dev@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          email
+        </a>
+        <a
+          href="https://www.linkedin.com/in/vqiu25"
+          target="_blank"
+          rel="noreferrer"
+        >
+          linkedin
+        </a>
+        <a href="https://github.com/vqiu25" target="_blank" rel="noreferrer">
+          github
+        </a>
+      </div>
+    </section>
   );
 }

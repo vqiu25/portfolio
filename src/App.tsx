b86fa3a65b4481e29cd154d3c1a48e3d6d447442
Victor Esther Qiu <vqiu25@gmail.com>
2025-11-06 22:16:49 +1300

    ✨ add smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import "./App.css";
+import { ReactLenis } from "lenis/react";
 import Header from "./components/app/header/Header";
 import Home from "./components/app/home/Home";
 import About from "./components/app/about/About";
 
 function App() {
   return (
-    <div className="appRoot">
-      <section className="appViewport">
-        <Header />
-        <main className="appContent">
-          <Home />
-        </main>
-      </section>
-      <About />
-    </div>
+    <>
+      <ReactLenis root />
+      <div className="appRoot">
+        <section className="appViewport">
+          <Header />
+          <main className="appContent">
+            <Home />
+          </main>
+        </section>
+        <About />
+      </div>
+    </>
   );
 }
 
